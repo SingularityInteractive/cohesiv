486fc3368381ad271394dbfe4fcf5741a95e0cff
Sam McCord <samuel.e.mccord@gmail.com>
2017-09-27 22:26:16 -0600

    Test go build precommit hook

--- installCli.sh
+++ installCli.sh
@@ -4,6 +4,7 @@ do
     echo "$element"
     go get "$element"
 done
+[ -z "$GOBIN" ] && export GOBINZ=$GOPATH/bin
 go install cohesiv.go
 echo "cohesiv Command Line Interface has been installed"
 cohesiv
